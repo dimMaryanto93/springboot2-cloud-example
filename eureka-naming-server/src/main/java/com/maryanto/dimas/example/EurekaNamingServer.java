009ecaca3574eee6135b1052166cae0ec0780274
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-07-27 14:27:18 +0700

    enabled eureka server & configure eureka dicovery service

--- eureka-naming-server/src/main/java/com/maryanto/dimas/example/EurekaNamingServer.java
+++ eureka-naming-server/src/main/java/com/maryanto/dimas/example/EurekaNamingServer.java
@@ -2,13 +2,13 @@ package com.maryanto.dimas.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class WilayahServiceApplication {
+@EnableEurekaServer
+public class EurekaNamingServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(WilayahServiceApplication.class, args);
+        SpringApplication.run(EurekaNamingServer.class, args);
     }
 }
--- nasabah-service/src/main/java/com/maryanto/dimas/example/NasabahServiceApplication.java
+++ nasabah-service/src/main/java/com/maryanto/dimas/example/NasabahServiceApplication.java
@@ -2,9 +2,11 @@ package com.maryanto.dimas.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NasabahServiceApplication {
 
