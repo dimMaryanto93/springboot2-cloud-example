b7416d08082d10f70c88a51ca84f41e1cdde8bca
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-07-27 17:10:18 +0700

    enabled api gateway

---
+++ api-gateway-zuul/src/main/java/com/maryanto/dimas/example/filter/ZuulLoggingFileter.java
@@ -0,0 +1,38 @@
+package com.maryanto.dimas.example.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulLoggingFileter extends ZuulFilter {
+
+    private final Logger console = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        console.info("the request url -> {}", request.getRequestURI());
+        return null;
+    }
+}
